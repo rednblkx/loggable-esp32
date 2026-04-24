04c00b07558ecb582d99dfbd61ff8ee93c117876
White Red <rednblkx@protonmail.com>
2026-04-24 15:35:42 +0300

    perf: lower default stack size config

--- include/loggable.hpp
+++ include/loggable.hpp
@@ -125,7 +125,7 @@ struct SinkerMetrics {
  * @brief Configuration for the async dispatch system.
  */
 struct SinkerConfig {
-  size_t task_stack_size = 4096;
+  size_t task_stack_size = 3580;
   int task_priority = 5;
   int task_core = -1; ///< -1 = any core
 };
